81171f460a7e413ec1a3da322c50cc43fde28a08
stolari <mpstolarek@gmail.com>
2019-11-16 12:23:51 +0100

    First commit

---
+++ src/main/java/com/mnm/JuniorKnowledgeBaseTool/JuniorKnowledgeBaseToolApplication.java
@@ -0,0 +1,13 @@
+package com.mnm.JuniorKnowledgeBaseTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorKnowledgeBaseToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorKnowledgeBaseToolApplication.class, args);
+	}
+
+}
