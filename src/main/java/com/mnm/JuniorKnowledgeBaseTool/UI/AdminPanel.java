952bf5209e42afd2d737cd041bce4db5c2976351
stolari <mpstolarek@gmail.com>
2019-11-18 19:37:31 +0100

    Filtering user fields on admin panel

--- src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/AdminPanel.java
+++ src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/AdminPanel.java
@@ -5,8 +5,13 @@ import com.mnm.JuniorKnowledgeBaseTool.repositories.UserRepoImpl;
 import com.mnm.JuniorKnowledgeBaseTool.repositories.UserRepository;
 import com.mnm.JuniorKnowledgeBaseTool.services.AdminPanelService;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.HeaderRow;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 
@@ -21,7 +26,55 @@ public class AdminPanel extends VerticalLayout {
         this.gridService = gridService;
 
         Grid<User> userGrid = new Grid<>(User.class);
-        userGrid.setItems(gridService.userInit());
+        ListDataProvider<User> dataProvider = new ListDataProvider<>(gridService.userInit());
+        userGrid.setDataProvider(dataProvider);
+        userGrid.removeAllColumns();
+        Grid.Column<User> userIdColumn = userGrid
+                .addColumn(User::getId).setHeader("Id");
+        Grid.Column<User> userLoginColumn = userGrid
+                .addColumn(User::getLogin).setHeader("Login");
+        Grid.Column<User> userEmailColumn = userGrid
+                .addColumn(User::getEmail).setHeader("Email");
+        Grid.Column<User> userRoleColumn = userGrid
+                .addColumn(User::getRole).setHeader("Role");
+        //userGrid.setColumns(userLoginColumn, userEmailColumn, userRoleColumn);
+
+        HeaderRow filterRow = userGrid.appendHeaderRow();
+        //Id Filter
+        TextField idField = new TextField();
+        idField.addValueChangeListener(event -> dataProvider.addFilter(user -> StringUtils.containsIgnoreCase(user.getId().toString(), idField.getValue())));
+        idField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        filterRow.getCell(userIdColumn).setComponent(idField);
+        idField.setSizeFull();
+        idField.setPlaceholder("Filter");
+        //login Filter
+        TextField loginField = new TextField();
+        loginField.addValueChangeListener(event -> dataProvider.addFilter(user -> StringUtils.containsIgnoreCase(user.getLogin(), loginField.getValue())));
+        loginField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        filterRow.getCell(userLoginColumn).setComponent(loginField);
+        loginField.setSizeFull();
+        loginField.setPlaceholder("Filter");
+
+        //Email Filter
+        TextField emailField = new TextField();
+        emailField.addValueChangeListener(event -> dataProvider.addFilter(user -> StringUtils.containsIgnoreCase(user.getEmail(), emailField.getValue())));
+        emailField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        filterRow.getCell(userEmailColumn).setComponent(emailField);
+        emailField.setSizeFull();
+        emailField.setPlaceholder("Filter");
+
+        //Role Filter
+        TextField roleField = new TextField();
+        roleField.addValueChangeListener(event -> dataProvider.addFilter(user -> StringUtils.containsIgnoreCase(user.getRole(), roleField.getValue())));
+        roleField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        filterRow.getCell(userRoleColumn).setComponent(roleField);
+        roleField.setSizeFull();
+        roleField.setPlaceholder("Filter");
+
 
         add(userGrid);
     }
