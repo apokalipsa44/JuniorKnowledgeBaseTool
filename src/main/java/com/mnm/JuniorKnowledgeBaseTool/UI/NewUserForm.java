f176b6766265cfee14f787e0307a7d76c545b52e
apokalipsa44 <m_lachowicz@wp.pl>
2019-11-19 14:23:28 +0100

    fixed max connections, fixed login form

--- src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/NewUserForm.java
+++ src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/NewUserForm.java
@@ -1,14 +1,60 @@
 package com.mnm.JuniorKnowledgeBaseTool.UI;
 
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
 
 @Route("newuserform")
-public class NewUserForm extends VerticalLayout {
+@Theme(value = Lumo.class, variant = Lumo.LIGHT)
+@HtmlImport("styles/shared-styles.css")
+public class NewUserForm extends FormLayout {
     public NewUserForm() {
-        Text text=new Text("create new user");
-        add(text);
 
+        setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1));
+
+        Text text = new Text("create new user");
+
+        TextField login = new TextField("Enter login");
+
+        EmailField email = new EmailField("Email");
+        email.setClearButtonVisible(true);
+        email.setErrorMessage("Please enter a valid email address");
+        email.isRequiredIndicatorVisible();
+
+        PasswordField password = new PasswordField("Enter password");
+        password.setRevealButtonVisible(true);
+        password.setMinLength(6);
+        if(password.getValue().length()<6){
+            password.setErrorMessage("password must be at least 6 chars");
+        }
+        PasswordField passwordConfirm = new PasswordField("Confirm password");
+        passwordConfirm.setRevealButtonVisible(true);
+        passwordConfirm.setMinLength(6);
+        if(passwordConfirm.getValue().length()<6){
+            passwordConfirm.setErrorMessage("password must be at least 6 chars");
+        }
+
+        Button saveButton = new Button("Save");
+        Button clearButton = new Button("Clear");
+        clearButton.addClickListener(e->{
+            login.setValue("");
+            email.setValue("");
+            password.setValue("");
+            passwordConfirm.setValue("");
+        });
+        HorizontalLayout buttons = new HorizontalLayout(saveButton, clearButton);
+
+        add(login, email, password, passwordConfirm, buttons);
+        setMaxWidth("350px");
     }
 }
