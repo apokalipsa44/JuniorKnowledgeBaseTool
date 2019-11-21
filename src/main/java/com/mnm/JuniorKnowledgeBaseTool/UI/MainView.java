76fdbac16238f3e1df36ea51fdeb6d04069b6923
stolari <mpstolarek@gmail.com>
2019-11-21 18:36:12 +0100

    Temporary main view added

--- src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/MainView.java
+++ src/main/java/com/mnm/JuniorKnowledgeBaseTool/UI/MainView.java
@@ -4,39 +4,61 @@ import com.vaadin.flow.component.applayout.AbstractAppRouterLayout;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.AppLayoutMenu;
 import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.Router;
-import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.PWA;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 
 @Route
-@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+//@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+@Theme(Lumo.class)
 public class MainView extends AbstractAppRouterLayout {
 
     private final Tabs tabs= new Tabs();
 
     public MainView() {
-        Tab tab = new Tab("Tab three");
+        Tab tab = new Tab("Strona główna");
+
+
+        Tab tab1 = new Tab("Katalog");
+
+
+        Tab tab2 = new Tab("Logout");
+
+
+        /*Map<Tab, Component> tabsWithPage = new HashMap<>();
+        //tabsWithPage.put(tab, page1);
+        //tabsWithPage.put(tab1, page2);*/
         tabs.add(tab);
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        tabs.setHeight("200px");
-        getElement().appendChild(tab.getElement());
+        tabs.add(tab1);
+        tabs.add(tab2);
+        //Div pages = new Div(page1, page2);
+
+
+        /*Set<Component> showPage = Stream.of(page1)
+                .collect(Collectors.toSet());
+
+        tabs.addSelectedChangeListener(event -> {
+            showPage.forEach(page -> page.setVisible(false));
+            showPage.clear();
+            Component selectedPage = tabsWithPage.get(tabs.getSelectedTab());
+            selectedPage.setVisible(true);
+            showPage.add(selectedPage);
+        });*/
+
+        tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
+        //tabs.setHeight("200px");
+        tabs.setFlexGrowForEnclosedTabs(1);
+        getElement().appendChild(tabs.getElement());
     }
 
     @Override
     protected void configure(AppLayout appLayout, AppLayoutMenu appLayoutMenu) {
-        appLayout.setBranding(new Span("Buongiorno"));
+        appLayout.setBranding(new Span("JuniorDeveloperTools"));
         appLayoutMenu.addMenuItem(new AppLayoutMenuItem(tabs));
     }
 }
